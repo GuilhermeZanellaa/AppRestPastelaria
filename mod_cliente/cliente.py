c1e4ed00351ae12417837d6e2a8f278e24c2db34
guilhermezanella <guihdzanella@gmail.com>
2022-10-19 18:38:51 -0300

    exercicio recuperacao 19-10

--- mod_cliente/cliente.py
+++ mod_cliente/cliente.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, render_template
+
 bp_cliente = Blueprint(
     'cliente', __name__, url_prefix="/cliente", template_folder='templates')
 
 
 @bp_cliente.route('/')
 def formListaCliente():
-
     return render_template('formListaCliente.html'), 200
+
+
+@bp_cliente.route('/form-cliente/', methods=['GET'])
+def formCliente():
+    return render_template('formCliente.html')
--- mod_produto/produto.py
+++ mod_produto/produto.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, render_template
+
 bp_produto = Blueprint(
     'produto', __name__, url_prefix="/produto", template_folder='templates')
 
 
-@bp_produto.route('/')
+@bp_produto.route('/', methods=['GET', 'POST'])
 def formListaProduto():
+    return render_template('formListaProduto.html')
 
-    return render_template('formListaProduto.html'), 200
+
+@bp_produto.route('/form-produto/', methods=['GET'])
+def formProduto():
+    return render_template('formProduto.html')
