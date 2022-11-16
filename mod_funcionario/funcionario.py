40667f910db3399b93f92999fae0e887b9203144
guilhermezanella <guihdzanella@gmail.com>
2022-11-16 19:58:35 -0300

    Funcoes e cifraSenha

--- mod_funcionario/funcionario.py
+++ mod_funcionario/funcionario.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from mod_funcoes import funcoes
 
 bp_funcionario = Blueprint(
     'funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
