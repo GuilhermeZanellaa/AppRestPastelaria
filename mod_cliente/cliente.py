b930085205a138b7885642b7f2fe1dd2c33f95db
guilhermezanella <guihdzanella@gmail.com>
2022-11-23 22:50:56 -0300

    CRUD Concluido, Bug na entrada de foto

--- mod_cliente/cliente.py
+++ mod_cliente/cliente.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from mod_funcoes.funcoes import Funcoes
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 import requests
-
-bp_cliente = Blueprint(
-    'cliente', __name__, url_prefix="/cliente", template_folder='templates')
+from mod_funcoes.funcoes import Funcoes
 
 
+bp_cliente = Blueprint('cliente', __name__,
+                       url_prefix="/cliente", template_folder='templates')
+
+''' endereços do endpoint '''
 urlApiClientes = "http://localhost:8000/cliente/"
 urlApiCliente = "http://localhost:8000/cliente/%s"
 headers = {'x-token': 'abcBolinhasToken', 'x-key': 'abcBolinhasKey'}
+
+''' rotas dos formulários '''
 
 
 @bp_cliente.route('/', methods=['GET', 'POST'])
@@ -20,44 +23,89 @@ def formListaCliente():
             raise Exception(result)
         return render_template('formListaCliente.html', result=result)
     except Exception as e:
-        return render_template('formListaCliente', erro=e)
+        return render_template('formListaCliente.html', erro=e)
 
 
-@bp_cliente.route('/form-cliente/', methods=['GET', 'POST'])
+@bp_cliente.route('/form-cliente', methods=['GET', 'POST'])
 def formCliente():
-    return render_template('formCliente.html')
+    return render_template('formCliente.html'), 200
 
 
-@bp_cliente.route('/insert', methods=['POST'])
+@bp_cliente.route('/insert', methods=['GET', 'POST'])
 def insert():
     try:
+
+        id_cliente = 0
         nome = request.form['nome']
         cpf = request.form['cpf']
         telefone = request.form['telefone']
-        compra_fiado = request.form['compra_fiado']
-        dia_fiado = request.form['dia_fiado']
-        if (compra_fiado == 0):
-            dia_fiado = None
+        dia_fiado = 5
+        compra_fiado = True
+        senha = Funcoes.cifraSenha(request.form['senha'])
 
-        senha = Funcoes.cifrarSenha(request.form['senha'])
+        payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'telefone': telefone,
+                   'dia_fiado': dia_fiado, 'senha': senha, 'compra_fiado': compra_fiado}
 
-        if 'compra_fiado' in request.form:
-            dia_fiado = request.form['compra_fiado']
-
-        payload = {
-            'nome': nome,
-            'cpf': cpf,
-            'telefone': telefone,
-            'senha': senha,
-            'compra_fiado': compra_fiado,
-            'dia_fiado': dia_fiado
-        }
-
-        response = requests.post(
-            urlApiClientes, headers=headers, json=payload)
+        response = requests.post(urlApiClientes, headers=headers, json=payload)
         result = response.json()
-
         return redirect(url_for('cliente.formListaCliente', msg=result))
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e)
+
+
+@bp_cliente.route("/form-edit-cliente", methods=['POST'])
+def formEditCliente():
+    try:
+
+        id_cliente = request.form['id']
+
+        response = requests.get(urlApiClientes + id_cliente, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+
+        return render_template('formCliente.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+
+@bp_cliente.route('/edit', methods=['POST'])
+def edit():
+    try:
+
+        id_cliente = request.form['id']
+        nome = request.form['nome']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        dia_fiado = 5
+        compra_fiado = True
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        payload = {'id_cliente': id_cliente, 'nome': nome, 'cpf': cpf, 'telefone': telefone,
+                   'dia_fiado': dia_fiado, 'senha': senha, 'compra_fiado': compra_fiado}
+
+        response = requests.put(
+            urlApiClientes + id_cliente, headers=headers, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
+    except Exception as e:
+        return render_template('formListaCliente.html', msgErro=e.args[0])
+
+
+@bp_cliente.route('/delete', methods=['POST'])
+def delete():
+    try:
+
+        id_cliente = request.form['id_cliente']
+
+        response = requests.delete(
+            urlApiClientes + id_cliente, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect(url_for('cliente.formListaCliente', msg=result[0]))
 
     except Exception as e:
-        return redirect(url_for('cliente.formListaCliente', msgErro=e))
+        return render_template('formListaCliente.html', msgErro=e.args[0])
--- mod_funcionario/funcionario.py
+++ mod_funcionario/funcionario.py
@@ -12,8 +12,6 @@ urlApiFuncionario = "http://localhost:8000/funcionario/%s"
 headers = {'x-token': 'abcBolinhasToken', 'x-key': 'abcBolinhasKey'}
 
 ''' rotas utilizando as variáveis '''
-
-
 @bp_funcionario.route('/', methods=['GET', 'POST'])
 def formListaFuncionario():
     try:
@@ -25,37 +23,80 @@ def formListaFuncionario():
     except Exception as e:
         return render_template('formListaFuncionario.html', erro=e)
 
-
-@bp_funcionario.route('/form-funcionario/', methods=['GET'])
+@bp_funcionario.route('/form-funcionario/', methods=['POST'])
 def formFuncionario():
     return render_template('formFuncionario.html')
 
-
-@bp_funcionario.route('/insert', methods=['POST'])
+@bp_funcionario.route('/insert', methods=['GET','POST'])
 def insert():
     try:
-        # atribui os dados do form para as variáveis
+
+        id_funcionario = 0
         nome = request.form['nome']
         matricula = request.form['matricula']
         cpf = request.form['cpf']
         telefone = request.form['telefone']
         grupo = request.form['grupo']
-        senha = Funcoes.cifrarSenha(request.form['senha'])
+        senha = Funcoes.cifraSenha(request.form['senha'])
 
-        # criação do objeto para ser enviado a API
-        payload = {
-            'nome': nome,
-            'matricula': matricula,
-            'cpf': cpf,
-            'telefone': telefone,
-            'grupo': grupo,
-            'senha': senha
-        }
+        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula,'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
 
-        response = requests.post(
-            urlApiFuncionarios, headers=headers, json=payload)
+        response = requests.post(urlApiFuncionarios, headers=headers, json=payload)
         result = response.json()
-
-        return redirect(url_for('funcionario.formListaFuncionario', msg=result))
+        return redirect( url_for('funcionario.formListaFuncionario', msg=result) )
     except Exception as e:
         return render_template('formListaFuncionario.html', msgErro=e)
+    
+@bp_funcionario.route("/form-edit-funcionario", methods=['POST'])
+def formEditFuncionario():
+    try:
+
+        id_funcionario = request.form['id']
+
+        response = requests.get(urlApiFuncionarios + id_funcionario, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+
+        return render_template('formFuncionario.html', result=result[0])
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+    
+@bp_funcionario.route('/edit', methods=['POST'])
+def edit():
+    try:
+
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula,'cpf': cpf, 'telefone':
+
+        telefone, 'grupo': grupo, 'senha': senha}
+
+        response = requests.put(urlApiFuncionarios + id_funcionario, headers=headers, json=payload)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect( url_for('funcionario.formListaFuncionario', msg=result[0]) )
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+    
+@bp_funcionario.route('/delete', methods=['POST'])
+def delete():
+    try:
+
+        id_funcionario = request.form['id_funcionario']
+
+        response = requests.delete(urlApiFuncionarios + id_funcionario, headers=headers)
+        result = response.json()
+        if (response.status_code != 200 or result[1] != 201):
+            raise Exception(result[0])
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
